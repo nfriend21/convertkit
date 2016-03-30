6a16cd9aa58c791f14d2436bbc10f9563ea00e61
Nick <nickmfriend@gmail.com>
2016-03-29 21:51:01 -0500

    remove pry

--- lib/convertkit/form.rb
+++ lib/convertkit/form.rb
@@ -36,7 +36,6 @@ module ConvertKit
 
     def subscribe(params)
       opts = {email: nil, fname: nil, course_opted: true}.merge(params)
-      binding.pry
       @client.post_request("/forms/#{@id}/subscribe", opts)
     end
 
